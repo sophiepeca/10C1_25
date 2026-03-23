c1382d4788c2075ade4f93f099e8389eed08d960
helmo2019 <helmo2019@yahoo.com>
2026-03-23 10:25:52 +0100

    KampfScene kompilierbar machen
    Ach Leute von heute

--- src/main/java/czg/scenes/KampfScene.java
+++ src/main/java/czg/scenes/KampfScene.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package czg.scenes;
 import czg.objects.BackdropObject;
+import czg.util.Images;
 
 /**
- *
- * @author guest-cgqpg1
+ * @author Sophie
  */
 public class KampfScene extends BaseScene{
     public KampfScene(){
         //Einfügen des Hintergrunds
         objects.add(new BackdropObject(Images.get("/assets/background/Kampfgang.png")));
         
-        //
-        objects.add(new InventarObject(Images.get("/assets/minigames/general/button_exit.png")));
+        SceneStack.INSTANCE.push(new InventarScene());
     }
 }
 
